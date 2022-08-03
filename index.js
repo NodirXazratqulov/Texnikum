d44034b6d55b8bf6d7c82c3b7a922d9027402f59
NodirXazratqulov <nodirxazratqulov2928@gmail.com>
2022-08-03 12:37:38 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+function check() {
+    let kunTunColor = document.getElementById('show').style.background;
+    if (kunTunColor == '#333') {
+        document.getElementById('show').style.background = 'white';
+        document.getElementById('show').style.color = '#333';
+    } else{
+        document.getElementById('show').style.background = '#333';
+        document.getElementById('show').style.color = 'white';
+    }
+}
+
+function check1() {
+    let kunTunColor1 = document.getElementById('show1').style.background;
+    if (kunTunColor1 == '#333') {
+        document.getElementById('show1').style.background = 'white';
+        document.getElementById('show1').style.color = '#333';
+    } else{
+        document.getElementById('show1').style.background = '#333';
+        document.getElementById('show1').style.color = 'white';
+    }
+}
+
+function check2() {
+    let kunTunColor2 = document.getElementById('show2').style.background;
+    if (kunTunColor2 == '#333') {
+        document.getElementById('show2').style.background = 'white';
+        document.getElementById('show2').style.color = '#333';
+    } else{
+        document.getElementById('show2').style.background = '#333';
+        document.getElementById('show2').style.color = 'white';
+    }
+}
+
+function check3() {
+    let kunTunColor3 = document.getElementById('show3').style.background;
+    if (kunTunColor3 == '#333') {
+        document.getElementById('show3').style.background = 'white';
+        document.getElementById('show3').style.color = '#333';
+    } else{
+        document.getElementById('show3').style.background = '#333';
+        document.getElementById('show3').style.color = 'white';
+    }
+}
+
+
+
+
